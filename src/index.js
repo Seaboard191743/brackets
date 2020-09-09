b8030f880b52086f1bfdba0162110a34aacc09c5
Usevalad Shychanin <exporesource@gmail.com>
2020-09-09 16:42:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj = {}
+  bracketsConfig.forEach((item, i) => obj[i] = item.join(''))
+  for(let i = 0; i < str.length; i+=1) {
+    if(str.includes(obj[i])) {
+      str = str.replace(obj[i], '')
+      i = -1
+    }
+  }
+  return !str ? true : false
 }
